cfb1872dd7799ef4d5aaed112aa6830a88e3870e
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-09-13 01:52:51 +0500

    design tool section full finshed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
-import * as selectors from './selectors.js'
-selectors.dropDownMenuLi.addEventListener("mouseover",() => {
-    selectors.dropDownMenu.style.display = 'flex';
-})
-selectors.dropDownMenuLi.addEventListener("mouseout",() => {
-    selectors.dropDownMenu.style.display = 'none';
-})
+import Swiper from "../node_modules/swiper/swiper-bundle.min.mjs";
+import * as selectors from "./selectors.js";
+//===========>> Swiper in tool section <<============//
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next-btn",
+    prevEl: ".prev-btn",
+  },
+});
+
+//==========>> droop Down menu hover <<=======================//
+selectors.dropDownMenuLi.addEventListener("mouseover", () => {
+  selectors.dropDownMenu.style.display = "flex";
+});
+selectors.dropDownMenuLi.addEventListener("mouseout", () => {
+  selectors.dropDownMenu.style.display = "none";
+});
+// ==========================================================//
