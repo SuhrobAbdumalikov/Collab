d90184c46a57a9cbd4e755e32b8dede2343ee556
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-09-16 02:39:54 +0500

    simple call section finished with tabled media

--- js/index.js
+++ js/index.js
@@ -25,38 +25,38 @@ selectors.dropDownMenuLi.addEventListener("mouseout", () => {
 // ==========================================================//
 
 //========>> toggle btn <<==============//
-selectors.togglebtn.addEventListener('click',() => {
-  selectors.togglebtn.classList.toggle('active');
-  selectors.monthlyBilled.classList.toggle('active');
-  selectors.yearlyBilled.classList.toggle('active');
-})
+selectors.togglebtn.addEventListener("click", () => {
+  selectors.togglebtn.classList.toggle("active");
+  selectors.monthlyBilled.classList.toggle("active");
+  selectors.yearlyBilled.classList.toggle("active");
+});
 
 //==========>> Testimonials swiper <<==================//
 const swiperTestimonial = new Swiper(".myTestimonialSwiper", {
   slidesPerView: 2,
   speed: 1000,
   grid: {
-    rows: 2
+    rows: 2,
   },
   spaceBetween: 30,
   navigation: {
     nextEl: ".nextBtn",
-    prevEl: ".prevBtn"
-  }
+    prevEl: ".prevBtn",
+  },
 });
 
 //==========>> Feature Box swiper <<===============//
 const FeatureBoxSwiper = new Swiper(".FeatureboxSwiper", {
   slidesPerView: 1,
-  speed:800,
+  speed: 800,
   grid: {
-    rows: 2
+    rows: 2,
   },
   spaceBetween: 20,
   navigation: {
     nextEl: ".nextFeatureBtn",
-    prevEl: ".prevFeatureBtn"
-  }
+    prevEl: ".prevFeatureBtn",
+  },
 });
 
 //=============>> Feature slide Box swiper <<=============//
@@ -65,7 +65,7 @@ const FeatureSlideSwiper = new Swiper(".FeatureSlideSwiper", {
   spaceBetween: 26,
   loop: true,
   speed: 1000,
-  autoplay:{
+  autoplay: {
     delay: 2000,
     disableOnInteraction: false,
   },
@@ -80,12 +80,12 @@ const verticalSwiper = new Swiper(".VerticalSwiper", {
   direction: "vertical",
   speed: 2000,
   loop: true,
-  autoplay:{
-    delay:2000,
+  autoplay: {
+    delay: 2000,
     disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
-  }
+    clickable: true,
+  },
 });
